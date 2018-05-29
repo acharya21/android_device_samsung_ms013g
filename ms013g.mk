4f019fd3f35cce77f6b24e68da612d3f6e7308e5
HASH <hash.xda@gmail.com>
2018-05-29 19:54:10 +0530

    msm8226-common: wifi: Copy WCNSS_qcom_cfg.ini to vendor partition
    * Required for O.
    
    Signed-off-by: HASH <hash.xda@gmail.com>

--- ms013g.mk
+++ ms013g.mk
@@ -48,7 +48,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     kernel/samsung/ms013g/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    kernel/samsung/ms013g/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    kernel/samsung/ms013g/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/vendor/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 # Default.prop overrides to get adb working at boot
