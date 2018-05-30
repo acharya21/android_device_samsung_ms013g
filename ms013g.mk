f8ce187fdd6df5ecf3e060fff151153a4ac82325
HASH <hash.xda@gmail.com>
2018-05-30 09:19:05 +0530

    ms013g: Move audio and media configs to vendor
    Signed-off-by: HASH <hash.xda@gmail.com>

--- ms013g.mk
+++ ms013g.mk
@@ -22,9 +22,9 @@ $(call inherit-product, vendor/samsung/ms013g/ms013g-vendor.mk)
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
     
 #GPS
 PRODUCT_COPY_FILES += \
@@ -32,7 +32,7 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
